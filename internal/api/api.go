981e832527dd5028e8328e22b28e5f30a3d8aad5
devevandro <dev.evandro@gmail.com>
2024-08-06 23:03:41 -0300

    feat: create routes

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rocketseat-education/semana-tech-go-react-server/internal/store/pgstore"
 )
 
@@ -20,7 +21,18 @@ func NewHandler(queries *pgstore.Queries) http.Handler {
 	api := apiHandler{queries: queries}
 
 	router := chi.NewRouter()
+	router.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	router.Route("/api", func(routerApi chi.Router) {
+		routerApi.Route("/rooms", func(routerRooms chi.Router) {
+			routerRooms.Post("/", api.handleCreateRoom)
+			routerRooms.Get("/", api.handleGetRooms)
+		})
+	})
 
 	api.router = router
 	return api
 }
+
+func (handler apiHandler) handleCreateRoom(writter http.ResponseWriter, request *http.Request) {}
+func (handler apiHandler) handleGetRooms(writter http.ResponseWriter, request *http.Request)   {}
